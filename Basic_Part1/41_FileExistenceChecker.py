bfc1213393c68dd981039593c4febe036efdbd57
alybt <alelybitun.school@gmail.com>
2026-03-18 21:02:26 +0800

    Exr: 41: FileExistenceChecker

--- Basic_Part1/41_FileExistenceChecker.py
+++ Basic_Part1/41_FileExistenceChecker.py
@@ -1,4 +1,22 @@
 import os.path
 
+# Solution 1
 print(os.path.isfile('main.txt'))
 print(os.path.isfile('main.py'))
+
+# Solution 2
+print(os.path.exists('main.txt'))
+print(os.path.exists('main.py'))
+
+# Solution 3
+print(os.getcwd())
+
+try: 
+    my_file = open('main.py')
+    my_file.close()
+    print("File found!")
+except FileNotFoundError:
+    print("File not found!")
+
+
+
