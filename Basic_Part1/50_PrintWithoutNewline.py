bbfcadb24321c722d2f08292e11b8dd19acd071b
alybt <alelybitun.school@gmail.com>
2026-03-19 08:46:18 +0800

    Exr: 50: Print Without Newline: Solution 2 and 3

--- Basic_Part1/50_PrintWithoutNewline.py
+++ Basic_Part1/50_PrintWithoutNewline.py
@@ -1,4 +1,20 @@
+import functools
+
+# Solution 1
 for i in range(0, 10):
     print('*', end = "")
 
-print("\n")
+print("\n") 
+
+# Solution 2
+printf = functools.partial(print, end="")
+
+for i in range(0,10):
+    printf('*')
+
+# Solution 3
+print("\n") 
+i = 0 
+while i < 10:
+    print('*', end = '')
+    i = i + 1
