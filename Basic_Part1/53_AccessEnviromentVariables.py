7fc78937e424df9affcf4946afe011630a22bfe3
alybt <alelybitun.school@gmail.com>
2026-03-19 09:00:04 +0800

    Exr: 53: Access Enviroment Variables: Solution 2 and 3

--- Basic_Part1/53_AccessEnviromentVariables.py
+++ Basic_Part1/53_AccessEnviromentVariables.py
@@ -1,9 +1,25 @@
 import os 
 
+# Solution 1
 print('*----------------------------------*')
 print(os.environ)
 print('*----------------------------------*')
-print(os.environ['HOME'])
+print(os.environ['USERPROFILE'])
 print('*----------------------------------*')
 print(os.environ['PATH'])
 print('*----------------------------------*')
+
+print('\n')
+# Solution 2
+for data in os.environ:
+    print(data)
+    print('-' * 15)
+    print(os.environ[data])
+    print('=' * 30 )
+
+
+print('\n')
+# Solution 3
+
+for item, value in os.environ.items():
+    print('{}: {}'.format(item, value))
